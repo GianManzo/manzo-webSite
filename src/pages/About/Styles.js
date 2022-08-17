4515bc9db8dad1c28f37fbf4193feb6ecbfb7706
Gian Manzo <ggianmanzo@gmail.com>
2022-08-17 16:17:01 -0300

    refactory page about and convert px from rem

--- src/pages/About/Styles.js
+++ src/pages/About/Styles.js
@@ -2,20 +2,41 @@ import styled from 'styled-components'
 import { gray, purple } from '../../_variables/_colors'
 
 export const Wrapper = styled.div`
-  margin: 24px;
+  margin: 1.5rem;
+
   :hover {
     cursor: default;
+  }
+`
+export const Grid = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    div.text-soudev {
+      grid-area: textSouDev;
+    }
+
+    display: grid;
+    max-width: 62.5rem;
+    margin: 0 auto;
+    grid-gap: 0 2rem;
+    grid-template-areas:
+      'myImage myText'
+      'textSouDev ImageSouDev';
   }
 `
 
 export const Image = styled.div`
   img {
-    width: 100%;
-    border-radius: 20px;
     border: 1px solid ${gray.g17};
-    margin-block: 24px;
-    max-height: 400px;
+    border-radius: 20px;
+    margin-block: 1.5rem;
+    max-height: 25rem;
+    width: 100%;
   }
+
   @media (min-width: 768px) {
     grid-area: myImage;
     justify-self: start;
@@ -24,19 +45,22 @@ export const Image = styled.div`
 export const Text = styled.div`
   p {
     color: ${gray.g4};
-    text-align: left;
-    font-size: 14px;
-    margin-bottom: 20px;
-    line-height: 25px;
     font-family: 'Roboto', sans-serif;
+    font-size: 0.875rem;
+    line-height: 1.5625rem;
+    margin-bottom: 1.25rem;
+    text-align: left;
+
     a {
       color: ${gray.g1};
       font-style: italic;
       transition: 0.3s;
+
       :hover {
         color: ${purple.p1};
       }
     }
+
     strong {
       color: ${gray.g1};
       font-weight: bold;
@@ -44,41 +68,26 @@ export const Text = styled.div`
   }
   @media (min-width: 768px) {
     grid-area: myText;
+
     p {
-      font-size: 16px;
+      font-size: 1rem;
     }
   }
 `
 
 export const ImageSouDev = styled.div`
-  margin-top: 80px;
-  margin-bottom: 24px;
-  width: 100%;
   display: flex;
-  max-width: 400px;
+  margin-top: 5rem;
+  margin-bottom: 1.5rem;
+  width: 100%;
+  max-width: 25rem;
+
   img {
     width: 100%;
   }
-  @media (min-width: 768px) {
-    justify-self: end;
-    grid-area: ImageSouDev;
-  }
-`
-export const Grid = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 
   @media (min-width: 768px) {
-    div.text-soudev {
-      grid-area: textSouDev;
-    }
-    display: grid;
-    max-width: 1000px;
-    margin: 0 auto;
-    grid-template-areas:
-      'myImage myText'
-      'textSouDev ImageSouDev';
-    grid-gap: 0 2rem;
+    grid-area: ImageSouDev;
+    justify-self: end;
   }
 `
