d04fbae4ed0fc4a26abe6aeab19240383ebadd0b
Gian Manzo <ggianmanzo@gmail.com>
2022-08-17 10:46:51 -0300

    refactory and adjusted card projects

--- src/components/CardProject/Styles.js
+++ src/components/CardProject/Styles.js
@@ -2,45 +2,48 @@ import styled from 'styled-components'
 import { gray, purple } from '../../_variables/_colors'
 
 export const Items = styled.li`
-  margin-bottom: 48px;
   display: flex;
   justify-content: center;
-  align-items: center;
+  margin-bottom: 48px;
 `
 
 export const Container = styled.div`
-  width: 100%;
-  height: auto;
-  max-width: 500px;
-  border-radius: 5px;
-  position: relative;
-
-  img {
-    border-radius: 5px;
-    width: 100%;
-  }
-  color: white;
   background-color: ${gray.g18};
-  opacity: 60%;
+  border-radius: 5px;
+  max-width: 500px;
+  opacity: 0.8;
+  position: relative;
   transition: 0.3s;
+
   :hover {
-    opacity: 80%;
-    cursor: default;
+    opacity: 1;
   }
+
   @media (min-width: 992px) {
     max-width: 1200px;
   }
 `
+export const Details = styled.div`
+  position: absolute;
+  left: -4px;
+  top: 50px;
+`
+
 export const Content = styled.div`
   padding: 1.5rem;
+
   @media (min-width: 992px) {
-    display: flex;
-    justify-content: center;
     align-items: center;
+    display: flex;
     gap: 4rem;
   }
 `
 export const Image = styled.div`
+  img {
+    border-radius: 5px;
+    width: 100%;
+  }
+
   @media (min-width: 992px) {
     max-width: 500px;
   }
@@ -51,18 +54,13 @@ export const Infos = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-gap: 44px;
   margin-block: 20px;
+
   h2 {
     color: ${gray.g1};
-    font-size: 25px;
     font-family: 'Fira Sans', sans-serif;
+    font-size: 25px;
     font-weight: bold;
     margin-bottom: 12px;
-  }
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    gap: 0.75rem;
   }
 `
 export const Descriptions = styled.div`
@@ -75,20 +73,21 @@ export const Descriptions = styled.div`
     line-height: 20px;
     margin-bottom: 20px;
   }
+
   a {
     text-decoration: none;
   }
 
   span {
-    transition: 0.3s;
     color: ${gray.g4};
-    margin-top: 20px;
     display: block;
-    padding: 0.75rem;
-    text-align: center;
     border: 1px solid ${purple.p9};
     border-radius: 5px;
     box-shadow: 0 1px 2px 0 ${purple.p4s};
+    margin-top: 20px;
+    padding: 0.75rem;
+    text-align: center;
+    transition: 0.3s;
 
     :hover {
       color: ${purple.p9};
@@ -96,14 +95,16 @@ export const Descriptions = styled.div`
   }
 `
 export const Techs = styled.div`
-  li {
-    padding: 10px;
-    background-color: ${gray.g14};
-    border-radius: 5px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+
+    li {
+      background-color: ${gray.g14};
+      border-radius: 5px;
+      color: ${gray.g1};
+      padding: 10px;
+    }
   }
-`
-export const Details = styled.div`
-  position: absolute;
-  top: 50px;
-  left: -4px;
 `
